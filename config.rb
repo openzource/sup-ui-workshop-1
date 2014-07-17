3a2b6f3505782c0d6235a8fc5405837e4d55d475
Rain Öpik <rain.opik@gmail.com>
2014-07-16 21:46:36 -0400

    Step 11 - migrated to Compass. Use 'compass --watch' to automatically rebuild CSS on changes in SCSS files. Edit config.rb to configure the SASS compiler. Added cross-browser border-radius to button class (with Compass)

---
+++ config.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+disable_warnings = true
+
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+generated_images_dir = "img_c"
+
+
+# Require any additional compass plugins here.
+
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
